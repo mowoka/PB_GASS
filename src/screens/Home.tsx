ac196d35ddab2eae278f0822956b8b47ecbdfca0
Giofanny Mowoka <giofanny@softwareseni.com>
2025-11-05 15:51:23 +0700

    feat: redirect create message to underconstuction

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -37,7 +37,7 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
     {
       name: 'Buat Pesan',
       image: WhatsApp,
-      onPress: () => {},
+      onPress: () => navigation.navigate('UnderConstruction'),
     },
     {
       name: 'Kontak Member',
